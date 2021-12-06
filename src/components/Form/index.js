cac361f2b21cbcf86e0fe6a8dbb6cdbb38aa7bf6
SashaMaksyutenko <84481245+SashaMaksyutenko@users.noreply.github.com>
2021-12-06 21:41:50 +0200

    changes commited

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,5 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
-import { Component } from "react";
-import PropTypes from "prop-types";
 
 import { Wrapper, Input, Row, Button, Comment } from "./styles";
 
